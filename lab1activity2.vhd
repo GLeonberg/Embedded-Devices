e08cfe5fe8415639bec87c251ed72ce3b180c863
Gregory Leonberg <GLeonberg@users.noreply.github.com>
2016-09-09 12:26:28 -0400

    Changed if ladder to switch statement

--- lab1activity2.vhd
+++ lab1activity2.vhd
@@ -15,72 +15,29 @@ end lab1activity2;
 architecture decode of lab1activity2 is
 begin
 
-	process (SW) is begin 
+	process (SW) is begin
+	
+		case SW is 
 
-		if (SW = "0000") then
-			HEX0 <= "0111111"; -- 0
-		end if;
-		
-		if (SW = "0001") then
-			HEX0 <= "0000110"; -- 1
-		end if;
-		
-		if (SW = "0010") then
-			HEX0 <= "1011011"; -- 2
-		end if;
-		
-		if (SW = "0011") then
-			HEX0 <= "1001111"; -- 3
-		end if;
-		
-		if (SW = "0100") then
-			HEX0 <= "1100110"; -- 4
-		end if;
-		
-		if (SW = "0101") then
-			HEX0 <= "1101101"; -- 5
-		end if;
-		
-		if (SW = "0110") then
-			HEX0 <= "1111101"; -- 6
-		end if;
-		
-		if (SW = "0111") then
-			HEX0 <= "0000111"; -- 7
-		end if;
-		
-		if (SW = "1000") then
-			HEX0 <= "1111111"; -- 8
-		end if;
-		
-		if (SW = "1001") then
-			HEX0 <= "1100111"; -- 9
-		end if;
-		
-		if (SW = "1010") then
-			HEX0 <= "1110111"; -- A
-		end if;
-		
-		if (SW = "1011") then
-			HEX0 <= "1111100"; -- b
-		end if;
-		
-		if (SW = "1100") then
-			HEX0 <= "0111001"; -- c
-		end if;
-		
-		if (SW = "1101") then 
-			HEX0 <= "1011110"; -- d
-		end if;
-		
-		if (SW = "1110") then
-			HEX0 <= "1111001"; -- E
-		end if;
-		
-		if (SW = "1111") then
-			HEX0 <= "1110001"; -- F
-		end if;
+			when "0000" => HEX0 <= "0111111"; -- 0
+			when "0001" => HEX0 <= "0000110"; -- 1
+			when "0010" => HEX0 <= "1011011"; -- 2
+			when "0011" => HEX0 <= "1001111"; -- 3
+			when "0100" => HEX0 <= "1100110"; -- 4
+			when "0101" => HEX0 <= "1101101"; -- 5
+			when "0110" => HEX0 <= "1111101"; -- 6
+			when "0111" => HEX0 <= "0000111"; -- 7
+			when "1000" => HEX0 <= "1111111"; -- 8
+			when "1001" => HEX0 <= "1100111"; -- 9
+			when "1010" => HEX0 <= "1110111"; -- A
+			when "1011" => HEX0 <= "1111100"; -- b
+			when "1100" => HEX0 <= "0111001"; -- c
+			when "1101" => HEX0 <= "1011110"; -- d
+			when "1110" => HEX0 <= "1111001"; -- E
+			when "1111" => HEX0 <= "1110001"; -- F
 
+		end case;
+		
 	end process;
 
 end decode;
